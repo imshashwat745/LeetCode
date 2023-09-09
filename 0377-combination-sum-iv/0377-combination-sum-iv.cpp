d0b3952b41e5e7635d591de07742e6b683575093
Shashwat Krishna Shukla <96909108+imshashwat745@users.noreply.github.com>
2023-09-09 12:35:22 +0530

    Time: 0 ms (100%), Space: 6.8 MB (20.91%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+private:
+    int help(vector<int> &arr,int t,vector<int> &dp){
+        if(t==0)return 1;
+        if(t<0)return 0;
+        if(dp[t]!=-1)return dp[t];
+        int ans=0;
+        for(auto i:arr){
+            ans+=help(arr,t-i,dp);
+        }
+        return dp[t]=ans;
+    }
+public:
+    int combinationSum4(vector<int>& arr, int t) {
+        vector<int> dp(t+1,-1);
+        return help(arr,t,dp);
+    }
+};
