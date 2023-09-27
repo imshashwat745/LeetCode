4c2e64c460bd853e2d0e6d3d09217f466afac401
Shashwat Krishna Shukla <96909108+imshashwat745@users.noreply.github.com>
2023-09-28 05:09:27 +0530

    Time: 237 ms (50%), Space: 38.3 MB (63.33%) - LeetHub

--- 3114-beautiful-towers-i/3114-beautiful-towers-i.kt
+++ 3114-beautiful-towers-i/3114-beautiful-towers-i.kt
@@ -2,6 +2,7 @@ class Solution {
     fun maximumSumOfHeights(arr: List<Int>): Long {
         var i=0
         var ans:Long=0
+        var maxi=0
         while(i<arr.size){
             var j=i-1
             var curr:Long=arr[i].toLong()
