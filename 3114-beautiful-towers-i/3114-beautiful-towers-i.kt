61eb17b4f311c29da1ef686dbae97baebaafefe0
Shashwat Krishna Shukla <96909108+imshashwat745@users.noreply.github.com>
2023-09-28 05:33:21 +0530

    Time: 241 ms (50%), Space: 39.8 MB (13.33%) - LeetHub

--- 3114-beautiful-towers-i/3114-beautiful-towers-i.kt
+++ 3114-beautiful-towers-i/3114-beautiful-towers-i.kt
@@ -1,25 +1,51 @@
 class Solution {
-    fun maximumSumOfHeights(arr: List<Int>): Long {
+    fun leftSmaller(arr: List<Int>): List<Int>{
+        var st=Stack<Int>()
+        var ans=ArrayList<Int>()
+        st.push(-1)
         var i=0
-        var ans:Long=0
-        var maxi=0
         while(i<arr.size){
-            var j=i-1
-            var curr:Long=arr[i].toLong()
-            var mini=arr[i]
-            while(j>=0){
-                mini=min(mini,arr[j])
-                curr+=mini.toLong()
-                --j
-            }
-            j=i+1
-            mini=arr[i]
-            while(j<arr.size){
-                mini=min(mini,arr[j])
-                curr+=mini.toLong()
-                ++j
-            }
-            ans=max(ans,curr)
+            while(st.size>1&&arr[st.peek()]>arr[i])st.pop()
+            ans.add(st.peek())
+            st.push(i)
+            ++i
+        }
+        return ans
+    }
+    fun rightSmaller(arr: List<Int>): List<Int>{
+        var st=Stack<Int>()
+        var ans=ArrayList<Int>()
+        st.push(arr.size)
+        var i=arr.size-1
+        while(i>-1){
+            while(st.size>1&&arr[st.peek()]>arr[i])st.pop()
+            ans.add(st.peek())
+            st.push(i)
+            --i
+        }
+        return ans.reversed()
+    }
+    fun maximumSumOfHeights(arr: List<Int>): Long {
+        var l=leftSmaller(arr)
+        var r=rightSmaller(arr)
+        var ls=Array<Long>(arr.size){0}
+        var rs=Array<Long>(arr.size){0}
+        var i=0
+        while(i<arr.size){
+            ls[i]=(1L)*(i-l[i])*arr[i]
+            if(l[i]!=-1)ls[i]+=ls[l[i]]
+            ++i;
+        }
+        i=arr.size-1
+        while(i>-1){
+            rs[i]=(1L)*(r[i]-i)*arr[i]
+            if(r[i]!=arr.size)rs[i]+=rs[r[i]]
+            --i;
+        }
+        var ans: Long=0
+        i=0
+        while(i<arr.size){
+            ans=max(ans,ls[i]+rs[i]-arr[i])
             ++i
         }
         return ans
