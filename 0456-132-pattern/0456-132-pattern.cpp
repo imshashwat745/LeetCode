4845f42daad83392741a306632c0f52ead8a6f18
Shashwat Krishna Shukla <96909108+imshashwat745@users.noreply.github.com>
2023-09-30 11:55:16 +0530

    Time: 408 ms (5.08%), Space: 64.3 MB (6.4%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,22 +1,31 @@
 class Solution {
 public:
-    bool find132pattern(vector<int>& nums) {
-        int n=size(nums), mini=1e9+8;
-        vector<int> l(n, 1e9+8), r(n, -1e9-8);
-        for(int i=0; i<n; i++){
-            if(mini<nums[i]) l[i]=mini;
-            mini=min(mini, nums[i]);
-        }
-        set<int> s;
-        for(int i=n-1; i>=0; i--){
-            auto it=s.lower_bound(nums[i]);
-            if(it!=begin(s)){
-                it--;
-                r[i]=*it;
+    bool find132pattern(vector<int>& arr) {
+        if(arr.size()<3)return false;
+        int mini=arr[0],j=1,k=2;
+        map<int,int> mp;
+        for(k=k;k<arr.size();++k)++mp[arr[k]];
+        while(j<arr.size()){
+            if(j>1){
+                --mp[arr[j]];
+                if(mp[arr[j]]<=0)mp.erase(arr[j]);
             }
-            s.insert(nums[i]);
+            if(mini>arr[j]){
+                mini=arr[j];
+                ++j;
+                continue;
+            }
+            auto it=mp.upper_bound(mini);
+            if(it!=mp.end()){
+                int ele=it->first;
+                cout<<mini<<" "<<arr[j]<<" "<<ele<<endl;
+                if(ele>mini&&ele<arr[j])return true;
+            }
+            
+
+            mini=min(mini,arr[j]);
+            ++j;
         }
-        for(int i=0; i<n; i++) if(l[i]<r[i]) return true;
         return false;
     }
 };
